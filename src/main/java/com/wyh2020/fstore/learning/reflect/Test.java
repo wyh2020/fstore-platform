05c826de7ddbd5726471136d17243835812f7879
of1410 <wangyonghua@qianmi.com>
2017-07-16 23:31:27 +0800

    反射

---
+++ src/main/java/com/wyh2020/fstore/learning/reflect/Test.java
@@ -0,0 +1,36 @@
+package com.wyh2020.fstore.learning.reflect;
+
+/**
+ * Created with wyh.
+ * Date: 2017/7/14
+ * Time: 下午5:35
+ */
+public class Test {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
